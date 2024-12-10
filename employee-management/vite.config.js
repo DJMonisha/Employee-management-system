aacd1463441c52e8d269fe206f16b464c92e82dd
DJMonisha <132485542+DJMonisha@users.noreply.github.com>
2024-12-10 16:24:45 +0000

    Dockerfile

--- employee-management/vite.config.js
+++ employee-management/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 4173,   // Ensure it matches the exposed port in the Dockerfile
+    host: '0.0.0.0',  // This allows the server to be accessed externally (e.g., from Docker container)
+  },
+});
